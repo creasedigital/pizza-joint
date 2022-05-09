fc138a0e2f04f3c03cd6d1f575f995bf0867a5e6
creasedigital <ojorechris@gmail.com>
2022-05-09 16:10:03 +0100

    basic animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
 	return (
-		<div className="home container">
-			<h2>Welcome to Pizza Joint</h2>
+		<motion.div className="home container" animate={{ x: -50 }}>
+			<motion.h2 animate={{ scale: 2.0 }}>Welcome to Pizza Joint</motion.h2>
 			<Link to="/base">
-				<button>Create Your Pizza</button>
+				<motion.button animate={{ scale: 1.5 }}>
+					Create Your Pizza
+				</motion.button>
 			</Link>
-		</div>
+		</motion.div>
 	);
 };
 
