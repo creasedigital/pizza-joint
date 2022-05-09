f2d904227b83146320627b24bd596abaf4dff9c6
creasedigital <ojorechris@gmail.com>
2022-05-09 19:37:21 +0100

    child variant

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,12 +1,43 @@
+import { motion } from "framer-motion";
+
 const Order = ({ pizza }) => {
+	const containerVariants = {
+		hidden: {
+			opacity: 0,
+			x: "-100vw",
+		},
+
+		visible: {
+			opacity: 1,
+			x: 0,
+			transition: { type: "spring", delay: 0.5 },
+		},
+	};
+	const childVariants = {
+		hidden: {
+			opacity: 0,
+		},
+
+		visible: {
+			opacity: 1,
+		},
+	};
+
 	return (
-		<div className="container order">
-			<h2>Thank you for your order :)</h2>
+		<motion.div
+			className="container order"
+			variants={containerVariants}
+			initial="hidden"
+			animate="visible"
+		>
+			<h2>Thank you for your order 😊</h2>
 			<p>You ordered a {pizza.base} pizza with:</p>
-			{pizza.toppings.map((topping) => (
-				<div key={topping}>{topping}</div>
-			))}
-		</div>
+			<motion.div variants={childVariants}>
+				{pizza.toppings.map((topping) => (
+					<div key={topping}>{topping}</div>
+				))}
+			</motion.div>
+		</motion.div>
 	);
 };
 
