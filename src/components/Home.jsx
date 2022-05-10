63bd534d65e7b5c5d32ca88748b504be29d7f797
creasedigital <ojorechris@gmail.com>
2022-05-10 13:55:16 +0100

    using keyframe on hover

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
 const buttonVariants = {
-	visible: {
-		x: [0, -20, 20, -20, 20, 0],
-		transition: { delay: 2 },
-	},
+	// visible: {
+	// 	x: [0, -20, 20, -20, 20, 0],
+	// 	transition: { delay: 2 },
+	// },
 	hover: {
-		scale: 1.1,
+		scale: [1, 1.1, 1, 1.1, 1, 1.1, 1, 1.1, 1],
 		textShadow: "0px 0px 8px rgb(255, 255, 255)",
 		boxShadow: "0px 0px 8px rgb(255, 255, 255)",
 	},
@@ -24,11 +24,7 @@ const Home = () => {
 				Welcome to Pizza Joint
 			</motion.h2>
 			<Link to="/base">
-				<motion.button
-					variants={buttonVariants}
-					animate="visible"
-					whileHover="hover"
-				>
+				<motion.button variants={buttonVariants} whileHover="hover">
 					Create Your Pizza
 				</motion.button>
 			</Link>
