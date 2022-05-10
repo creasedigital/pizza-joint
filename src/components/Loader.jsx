e033030839b9ffabb46be0a33480c433d1c5d70a
creasedigital <ojorechris@gmail.com>
2022-05-10 23:20:53 +0100

    useCycle hook added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
 const loaderVariants = {
 	animationOne: {
@@ -17,16 +17,31 @@ const loaderVariants = {
 			},
 		},
 	},
+	animationTwo: {
+		y: [0, -30],
+		x: 0,
+		transition: {
+			y: {
+				yoyo: Infinity,
+				duration: 0.25,
+				ease: "easeOut",
+			},
+		},
+	},
 };
 
 const Loader = () => {
+	const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
 	return (
 		<>
 			<motion.div
 				className="loader"
 				variants={loaderVariants}
-				animate="animationOne"
+				animate={animation}
 			></motion.div>
+			<div style={{ cursor: "pointer" }} onClick={() => cycleAnimation()}>
+				Cycle Loader
+			</div>
 		</>
 	);
 };
